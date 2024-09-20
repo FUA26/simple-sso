5cf80ff6ed739f94fd39b3b91278540834bd93a9
fatihua <fatihua@malangkab.go.id>
2024-09-20 09:12:52 +0700

    update login page

--- app/oauth/sso/authorize/page.tsx
+++ app/oauth/sso/authorize/page.tsx
@@ -1,67 +1,39 @@
+// import Image from "next/image";
 "use client";
-
-import { useSearchParams } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { AuthorizeForm } from "@/components/forms/authorize-form";
+import Image from "@/components/ui/image";
 import Link from "next/link";
 
-export const description =
-  "A simple login form with email and password. The submit button says 'Sign in'.";
-
-export default function LoginForm() {
-  const searchParams = useSearchParams();
-
-  const search = searchParams.get("name");
+export default function Login() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login {search}</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
-          </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
-          </div>
+    <>
+      <div className="grid gap-2 text-center">
+        <div className="relative m-auto  aspect-[4/1] w-full ">
+          <Image
+            className="rounded-2xl"
+            src="/images/full-login.png"
+            alt="Profile Image"
+            fill
+            enableTransition={false}
+            enableLoading={false}
+            priority={true}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
         </div>
+        <h1 className="text-3xl font-bold">Login</h1>
+        <p className="text-muted-foreground">
+          Enter your email below to login to your account
+        </p>
       </div>
-      <div className="hidden bg-muted lg:block"></div>
-    </div>
+      <div className="grid gap-4">
+        <AuthorizeForm />
+      </div>
+      <div className="mt-4 text-center text-sm">
+        Belum punya akun ?{" "}
+        <Link href="register" className="underline">
+          Daftar
+        </Link>
+      </div>
+    </>
   );
 }
